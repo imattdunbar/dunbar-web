d080838f6c272b3eb40ff29ec707b7ca4027da52
Matt Dunbar <me@mattdunbar.io>
2023-07-26 15:46:20 -0400

    Added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import '$lib/styles/global.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
