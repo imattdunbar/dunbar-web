1881f9455f436da9b91f5b65e4cc3a9e35219ee3
Matt Dunbar <me@mattdunbar.io>
2024-08-26 21:37:47 -0400

    Squashed

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<div class="h-screen w-full m-auto flex flex-col justify-center items-center">
+	<img class="w-[70%] rounded-2xl" src="/images/404.webp" alt="" />
+
+	<a class="text-blue text-2xl font-semibold mt-8" href="/">
+		<button class="btn bg-blue-600 hover:bg-blue-400 text-gray-300">Go Back Home</button>
+	</a>
+</div>
