a81c5954612d2c939dfac277a06fd1f34d1a2275
Matt Dunbar <me@mattdunbar.io>
2025-07-24 19:10:18 -0400

    html lang

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,7 +28,7 @@ function RootComponent() {
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <HeadContent />
       </head>
